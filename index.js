5430ded8a3e0c26c79b98dfcbea6f4b40909cf5b
Skyler H Gold <skylergold96@gmail.com>
2020-03-10 19:13:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipe = ();
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return object.assign({},object,{[key]: value})
+  return object.assign ({},object,{[key]: value})
 }
 
 //function destructivelyUpdateObjectWithKeyAndValue (object, key value//){
