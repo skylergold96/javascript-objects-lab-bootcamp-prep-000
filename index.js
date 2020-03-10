27807403522664176b7538c5b682b78cc50b897e
Skyler H Gold <skylergold96@gmail.com>
2020-03-10 19:04:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var object = {prop: 1};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return object.assign({},object,{[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key value){
+  
+}
+  
